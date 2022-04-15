21ee7d766493a422c98b95620a17a994427552bd
Irem Koc <irem-koc-75@hotmail.com>
2022-04-15 22:07:01 +0300

    third commit

---
+++ last_question.sql
@@ -0,0 +1,11 @@
+/*
+Code Implementation Task: If this list would be a database table, 
+please provide SQL query to fill in the missing daily vaccination 
+numbers with discrete median of country as similar to question a.  
+Please  provide the link to your code as answer to this question. 
+Note: This time SQL equivalent is requested, and imputation value is 
+median of each country, not minimum. Please remember filling countries
+with zero if they do not have any valid daily_vaccination records 
+like Kuwait.
+*/
+
